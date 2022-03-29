7a7079f1a98a411bc72f308cd5cbf1c074d90f71
manojv17 <89714748+manojv17@users.noreply.github.com>
2022-03-29 14:57:01 -0500

    Add files via upload

---
+++ Home.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+ 
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        <h2>HELLO</h2>
+        <p>Services to provide assistance with activities of daily living and self-management tasks to persons who reside in their own homes. In-home services include personal care, shopping, housekeeping, meal preparation, assistance with medication and transportation as an alternative to long-term care in a nursing facility. 
+          The In-Home Services Program is a waivered community-based care program.</p>
+ 
+        <p>Medicaid Services eligible based on age, blindness or disability and must require personal care assistance from another person without a more structured setting.</p>
+      </div>
+    );
+  }
+}
+ 
+export default Home;
---
+++ Stuff.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+ 
+class Stuff extends Component {
+  render() {
+    return (
+      <div>
+        <h2>STUFF</h2>
+        <p>Handyman & Home Repair Services</p>
+        <ol>
+          <li>Bathroom Repair & Installation Services</li>
+          <li>Facilisis bibendum</li>
+          <li>Kitchen Installation & Repair Services</li>
+          <li>Exterior Cleaning & Repair Servicest</li>
+          
+        </ol>
+      </div>
+    );
+  }
+}
+ 
+export default Stuff;
